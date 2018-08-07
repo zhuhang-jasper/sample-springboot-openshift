74e7e2a5970c5f1fae419386e38fe1b149dabb6f
zhuhang-jasper <zhuhang.jasper@gmail.com>
2018-08-07 17:31:11 +0800

    Add index.html & mapping for /home

--- src/main/java/io/openshift/booster/BoosterApplication.java
+++ src/main/java/io/openshift/booster/BoosterApplication.java
@@ -19,8 +19,10 @@ import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = { "com.web", "io.openshift.booster" })
 public class BoosterApplication {
 
     public static void main(String[] args) {
